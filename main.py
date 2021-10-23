441a7ebb9bddcabdb3cecfd5c1f80011f43ef984
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2021-10-23 22:28:38 +0300

    Add minimal working example

--- main.py
+++ main.py
@@ -1,5 +1,65 @@
+import torch
+
+from ds.dataset import PtbXlWrapper
+from ds.tensorboard import TensorboardExperiment
+from ds.runner import Runner, run_epoch
+from ds.models import ConvLinearBasicModel
+
+
+# Hyperparameters
+EPOCH_COUNT = 60
+LR = 6e-4
+BATCH_SIZE = 128
+LOG_PATH = "./runs"
+
+# Data confuguration
+DATA_DIR = "data"
+
+# Hardware configuration
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"{DEVICE=}")
+
+
 def main():
-    pass
+    sampling_rate = 100
+
+    dataset = PtbXlWrapper(DATA_DIR, sampling_rate, BATCH_SIZE)
+
+    # Create the data loaders
+    train_dl = dataset.make_train_dataloader()
+    test_dl = dataset.make_test_dataloader()
+
+    # Model and optimizer
+    model = ConvLinearBasicModel().to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+    # Create the runners
+    test_runner = Runner(test_dl, model, device=DEVICE)
+    train_runner = Runner(train_dl, model, optimizer, device=DEVICE)
+
+    # Setup the experiment tracker
+    tracker = TensorboardExperiment(LOG_PATH)
+
+    # Run the epochs
+    for epoch_id in range(EPOCH_COUNT):
+        run_epoch(test_runner, train_runner, tracker, epoch_id)
+
+        # Compute Average Epoch Metrics
+        summary = ", ".join(
+            [
+                f"[Epoch: {epoch_id + 1}/{EPOCH_COUNT}]",
+                f"Test Accuracy: {test_runner.avg_accuracy: 0.4f}",
+                f"Train Accuracy: {train_runner.avg_accuracy: 0.4f}",
+            ]
+        )
+        print("\n" + summary + "\n")
+
+        # Reset the runners
+        train_runner.reset()
+        test_runner.reset()
+
+        # Flush the tracker after every epoch for live updates
+        tracker.flush()
 
 
 if __name__ == "__main__":
